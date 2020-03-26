4e756c7c8b2ab5c00a2cfbcdee53aed3a6a5bc4a
Sumaer Jolly <sumaerjolly@gmail.com>
2020-03-26 14:53:54 +0530

    add routes and link for easy navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Photos from './pages/Photos';
+import Cart from './pages/Cart';
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Home Page</h1>
+      <Switch>
+        <Route exact path="/" component={Photos} />
+        <Route exact path="/cart" component={Cart} />
+      </Switch>
     </div>
   );
 }
