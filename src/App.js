e9dec0425ce7866eb695d515ba4f697abd0a0c76
Sumaer Jolly <sumaerjolly@gmail.com>
2020-03-26 14:33:18 +0530

    add header component and styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <Header />
+      <h1>Home Page</h1>
     </div>
   );
 }
