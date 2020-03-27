ec32c0ba7af30603e2bd250ee1044f4a476fe945
Sumaer Jolly <sumaerjolly@gmail.com>
2020-03-27 21:22:43 +0530

    add full trash icon on hover for UX improvement

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,13 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { PicsContext } from '../PicsContext';
 
 function CartItem({ item }) {
+  const [hovered, setHovered] = useState(false);
   const { removeFromCart } = useContext(PicsContext);
+
+  const iconClassName = hovered ? 'ri-delete-bin-fill' : 'ri-delete-bin-line';
+
   return (
     <div className="cart-item">
       <i
         onClick={() => removeFromCart(item.id)}
-        className="ri-delete-bin-line"
+        className={iconClassName}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
       ></i>
       <img src={item.url} width="130px" />
       <p>$5.99</p>
