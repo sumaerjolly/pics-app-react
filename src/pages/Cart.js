d02a69002834a9fd0943add70f4e3050e60be96f
Sumaer Jolly <sumaerjolly@gmail.com>
2020-03-27 20:53:24 +0530

    add caritems component to show cartitems on cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { PicsContext } from '../PicsContext';
+import CartItem from '../components/CartItem';
 
 function Cart() {
+  const { cartItems } = useContext(PicsContext);
+  const cartItemElements = cartItems.map(item => (
+    <CartItem key={item.id} item={item} />
+  ));
   return (
     <main className="cart-page">
       <h1>Check out</h1>
+      {cartItemElements}
+      <p className="total-cost">Total: </p>
+      <div className="order-button">
+        <button>Place Order</button>
+      </div>
     </main>
   );
 }
