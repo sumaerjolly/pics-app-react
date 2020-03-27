bd2fba73081f7e638ddb23e102e512cc244494d9
Sumaer Jolly <sumaerjolly@gmail.com>
2020-03-27 19:00:18 +0530

    add fill cart icon to rrender conditionally

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { PicsContext } from '../PicsContext';
 
 function Header() {
+  const { cartItems } = useContext(PicsContext);
+  const cartClassName = cartItems.length
+    ? 'ri-shopping-cart-fill ri-fw ri-2x'
+    : 'ri-shopping-cart-line ri-fw ri-2x';
   return (
     <header>
       <Link to="/">
         <h2>Pics App</h2>
       </Link>
       <Link to="/cart">
-        <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+        <i className={cartClassName}></i>
       </Link>
     </header>
   );
