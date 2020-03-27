45d04c3533a597ecf0f69f055dcdc18778f6eecb
Sumaer Jolly <sumaerjolly@gmail.com>
2020-03-27 21:08:40 +0530

    add remove from cart on cart page and total cost method

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -7,11 +7,16 @@ function Cart() {
   const cartItemElements = cartItems.map(item => (
     <CartItem key={item.id} item={item} />
   ));
+  const total = cartItemElements.length * 5.99;
+  const totalCostDisplay = total.toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
   return (
     <main className="cart-page">
       <h1>Check out</h1>
       {cartItemElements}
-      <p className="total-cost">Total: </p>
+      <p className="total-cost">Total: {totalCostDisplay} </p>
       <div className="order-button">
         <button>Place Order</button>
       </div>
